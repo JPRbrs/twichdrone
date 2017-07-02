ac9b675067c4e8c1de7aea967e99672019c4ad00
javier <perezrubio.javier@gmail.com>
2017-07-03 00:12:52 +0100

    PEP8 fixes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HPI=/home/pi
+HPI=$(pwd)
 SDIR=$HPI/mjpg-streamer/mjpg-streamer-experimental
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDIR
@@ -9,4 +9,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SDIR
 
 $SDIR/mjpg_streamer -o "output_http.so -w $HPI/TwichDrone/www -p 80" -i "input_raspicam.so -x 768 -y 432 -fps 25 -vf -hf" &
 
-cd $HPI/TwichDrone/python; python $HPI/TwichDrone/python/controlserver.py -a 192.168.2.1 -p 8000 -g rainbow6 -f 60 &
+# cd $HPI/TwichDrone/python; python $HPI/TwichDrone/python/controlserver.py -a 192.168.2.1 -p 8000 -g rainbow6 -f 60 &
+
+cd $HPI/TwichDrone/python; python $HPI/TwichDrone/python/controlserver.py -a 127.0.0.1 -p 8000 -g rainbow6 -f 60
