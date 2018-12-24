4261f36c87184457ecdd882de4751f158c0f0fe8
javier@wazoku <javier.perez.rubio@wazoku.com>
2018-12-24 02:00:21 +0000

    Testing different speed with analogWrite

--- TwichDrone/python/ardumotor_chip.py
+++ TwichDrone/python/ardumotor_chip.py
@@ -27,17 +27,21 @@ class ArduMotor(object):
     def move_motor(self, ml=None, mr=None):
         if ml and mr:
             # Activate motors
-            self.a.digitalWrite(MOTOR_LEFT_EN, self.a.HIGH)
-            self.a.digitalWrite(MOTOR_RIGHT_EN, self.a.HIGH)
 
             if ml.power > 0 and mr.power > 0:
                 if ml.direction == 1 and mr.direction == 1:
+                    self.a.analogWrite(MOTOR_LEFT_EN, 255)
+                    self.a.analogWrite(MOTOR_RIGHT_EN, 255)
+
                     self.a.digitalWrite(MOTOR_LEFT_FWD, self.a.HIGH)
                     self.a.digitalWrite(MOTOR_RIGHT_FWD, self.a.HIGH)
                     self.a.digitalWrite(MOTOR_LEFT_RV, self.a.LOW)
                     self.a.digitalWrite(MOTOR_RIGHT_RV, self.a.LOW)
 
                 if ml.direction == 0 and mr.direction == 0:
+                    self.a.analogWrite(MOTOR_LEFT_EN, 200)
+                    self.a.analogWrite(MOTOR_RIGHT_EN, 200)
+
                     self.a.digitalWrite(MOTOR_LEFT_FWD, self.a.LOW)
                     self.a.digitalWrite(MOTOR_RIGHT_FWD, self.a.LOW)
                     self.a.digitalWrite(MOTOR_LEFT_RV, self.a.HIGH)
